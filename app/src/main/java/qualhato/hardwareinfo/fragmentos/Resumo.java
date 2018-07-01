d52d05b90faf44bdb467384445569e23ef78a516
brunoqualhato <sadraqui7894>
2018-07-01 16:37:31 -0300

    AJUSTES NA BATERIA, TUDO OK

--- app/src/main/java/qualhato/hardwareinfo/fragmentos/Resumo.java
+++ app/src/main/java/qualhato/hardwareinfo/fragmentos/Resumo.java
@@ -1,6 +1,7 @@
 package qualhato.hardwareinfo.fragmentos;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
@@ -14,6 +15,8 @@ import android.widget.ListView;
 import java.util.ArrayList;
 
 import qualhato.hardwareinfo.R;
+import qualhato.hardwareinfo.atividades.MainActivity;
+import qualhato.hardwareinfo.hardware.ObterBateria;
 import qualhato.hardwareinfo.hardware.ObterHardwarePadrao;
 import qualhato.hardwareinfo.util.ConverteValores;
 
@@ -26,8 +29,9 @@ public class Resumo extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         ObterHardwarePadrao obterHardwarePadrao = new ObterHardwarePadrao();
-
+        ObterBateria obterBateria = new ObterBateria();
         View view = inflater.inflate(R.layout.fragment_resumo, container, false);
+
 
         ListView listaTela = (ListView) view.findViewById(R.id.listaResumo);
         ArrayList<String> lista = new ArrayList<>();
@@ -48,6 +52,10 @@ public class Resumo extends Fragment {
         lista.add("Usuario: " + obterHardwarePadrao.usuario());
         lista.add("SDK: " + obterHardwarePadrao.versaoSDK());
         lista.add(obterHardwarePadrao.obterWifi(getContext()));
+        lista.add(obterBateria.readBattery(getContext()));
+
+
+
 
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
                 getContext(),
--- app/src/main/java/qualhato/hardwareinfo/hardware/ObterBateria.java
+++ app/src/main/java/qualhato/hardwareinfo/hardware/ObterBateria.java
@@ -1,10 +1,13 @@
 package qualhato.hardwareinfo.hardware;
 
+import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.BatteryManager;
+import android.util.DebugUtils;
+import android.util.Log;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -12,61 +15,84 @@ import qualhato.hardwareinfo.R;
 
 public class ObterBateria {
 
-    private Context mContext;
+    public String readBattery(Context context) {
+        StringBuilder sb = new StringBuilder();
+        IntentFilter batteryIntentFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent batteryIntent = context.registerReceiver(null, batteryIntentFilter);
 
-    private TextView mTextViewInfo;
-    private TextView mTextViewPercentage;
-    private ProgressBar mProgressBar;
+        boolean present = batteryIntent.getExtras().getBoolean(BatteryManager.EXTRA_PRESENT);
+        sb.append("PRESENTE?: " + present + "\n");
 
-    private int mProgressStatus = 0;
+        int status = batteryIntent.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+        if (status == BatteryManager.BATTERY_STATUS_CHARGING) {
+            sb.append("BATERIA ESTÁ CARREGANDO\n");
+        }
+        if (status == BatteryManager.BATTERY_STATUS_FULL) {
+            sb.append("BATERIA ESTÁ CHEIA\n");
+        }
 
-    public String obterBateria(Context context){
-        String resultado = "";
+        int plugged = batteryIntent.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
+        if (plugged == BatteryManager.BATTERY_PLUGGED_USB) {
+            sb.append("BATERIA PLUGADA NA USB\n");
+        }
+        if (plugged == BatteryManager.BATTERY_PLUGGED_AC) {
+            sb.append("BATERIA PLUGADA NO AC\n");
+        }
 
-        // Get the application context
-        mContext = context.getApplicationContext();
+        int level = batteryIntent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+        sb.append("LEVEL: " + level + "\n");
 
-        IntentFilter iFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        int scale = batteryIntent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+        sb.append("ESCALA: " + scale + "\n");
 
-        mContext.registerReceiver(mBroadcastReceiver,iFilter);
-//
-//
-//        mTextViewInfo = (TextView) findViewById(R.id.tv_info);
-//        mTextViewPercentage = (TextView) findViewById(R.id.tv_percentage);
-//        mProgressBar = (ProgressBar) findViewById(R.id.pb);
+        int health = batteryIntent.getIntExtra(BatteryManager.EXTRA_HEALTH, 0);
+        sb.append("SAUDE: " + saudeBateria(health) + "\n");
+
+        String technology = batteryIntent.getExtras().getString(BatteryManager.EXTRA_TECHNOLOGY);
+        sb.append("TECNOLOGIA: " + technology + "\n");
+
+        int temperature = batteryIntent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, 0);
+        sb.append("TEMPERATURE: " + temperature + "\n");
+
+        int voltage = batteryIntent.getIntExtra(BatteryManager.EXTRA_VOLTAGE, 0);
+        sb.append("VOLTAGE: " + voltage + "\n");
 
 
-        return resultado;
+
+        int icon_small_resourceId = batteryIntent.getIntExtra(BatteryManager.EXTRA_ICON_SMALL, 0);
+        sb.append("ICONE PEQUENO: " + icon_small_resourceId + "\n");
+
+        return sb.toString();
     }
 
-    private BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-
-            int scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE,-1);
-            mTextViewInfo.setText("Battery Scale : " + scale);
-
-
-
-            int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL,-1);
-            // Display the battery level in TextView
-            mTextViewInfo.setText(mTextViewInfo.getText() + "\nBattery Level : " + level);
-
-            // Calculate the battery charged percentage
-            float percentage = level/ (float) scale;
-            // Update the progress bar to display current battery charged percentage
-            mProgressStatus = (int)((percentage)*100);
-
-            // Show the battery charged percentage text inside progress bar
-            mTextViewPercentage.setText("" + mProgressStatus + "%");
-
-            // Show the battery charged percentage in TextView
-            mTextViewInfo.setText(mTextViewInfo.getText() +
-                    "\nPercentage : "+ mProgressStatus + "%");
-
-            // Display the battery charged percentage in progress bar
-            mProgressBar.setProgress(mProgressStatus);
+    public String saudeBateria(int health) {
+        String result;
+        switch (health) {
+            case BatteryManager.BATTERY_HEALTH_COLD:
+                result = "BATERIA ESTÁ VELHA";
+                break;
+            case BatteryManager.BATTERY_HEALTH_DEAD:
+                result = "BATERIA ESTÁ QUASE MORTA";
+                break;
+            case BatteryManager.BATTERY_HEALTH_GOOD:
+                result = "BATERIA ESTÁ BOA";
+                break;
+            case BatteryManager.BATTERY_HEALTH_OVERHEAT:
+                result = "BATERIA ESTÁ OTIMA";
+                break;
+            case BatteryManager.BATTERY_HEALTH_OVER_VOLTAGE:
+                result = "BATERIA ESTÁ SOB SUPER TENSÃO";
+                break;
+            case BatteryManager.BATTERY_HEALTH_UNKNOWN:
+                result = "ESTADO DA BATERIA DESCONHECIDO";
+                break;
+            case BatteryManager.BATTERY_HEALTH_UNSPECIFIED_FAILURE:
+                result = "SAUDE DA BATERIA NÃO ESPECIFICADA";
+                break;
+            default:
+                result = "unkknown";
         }
-    };
 
+        return result;
+    }
 }
