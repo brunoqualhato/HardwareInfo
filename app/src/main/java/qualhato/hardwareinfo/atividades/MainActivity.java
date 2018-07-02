f9217d2c8a2c4ccdfe4c617034ff7f4a953733a6
brunoqualhato <sadraqui7894>
2018-07-02 18:23:18 -0300

    Mudanças no desing

--- app/src/main/java/qualhato/hardwareinfo/atividades/MainActivity.java
+++ app/src/main/java/qualhato/hardwareinfo/atividades/MainActivity.java
@@ -1,15 +1,9 @@
 package qualhato.hardwareinfo.atividades;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.os.BatteryManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -17,12 +11,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ProgressBar;
-import android.widget.TextView;
+import android.view.View;
 
 import qualhato.hardwareinfo.R;
+import qualhato.hardwareinfo.SIM;
+import qualhato.hardwareinfo.fragmentos.APPS;
+import qualhato.hardwareinfo.fragmentos.Bateria;
+import qualhato.hardwareinfo.fragmentos.Bluetooth;
+import qualhato.hardwareinfo.fragmentos.CPU;
+import qualhato.hardwareinfo.fragmentos.Camera;
+import qualhato.hardwareinfo.fragmentos.DetalhesSensores;
+import qualhato.hardwareinfo.fragmentos.Extras;
+import qualhato.hardwareinfo.fragmentos.Internet;
+import qualhato.hardwareinfo.fragmentos.Memoria;
 import qualhato.hardwareinfo.fragmentos.Processador;
 import qualhato.hardwareinfo.fragmentos.Resumo;
+import qualhato.hardwareinfo.fragmentos.SistemaOperacional;
+import qualhato.hardwareinfo.fragmentos.Sobre;
+import qualhato.hardwareinfo.fragmentos.Tela;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -57,7 +63,6 @@ public class MainActivity extends AppCompatActivity
         getSupportFragmentManager().beginTransaction()
                 .add(R.id.conteiner, new Resumo())
                 .commit();
-
 
 
     }
@@ -96,26 +101,66 @@ public class MainActivity extends AppCompatActivity
 
         int id = item.getItemId();
 
-        if (id == R.id.nav_resumo) {
+        if (id == R.id.nav_dispositivo) {
 
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.conteiner, new Resumo())
                     .commit();
 
-        } else if (id == R.id.nav_processador) {
+        } else if (id == R.id.nav_os) {
 
             getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.conteiner, new Processador())
+                    .replace(R.id.conteiner, new SistemaOperacional())
                     .commit();
 
-        } else if (id == R.id.nav_sensores) {
-
-        } else if (id == R.id.nav_teste) {
-
-        } else if (id == R.id.nav_teste1) {
-
-        } else if (id == R.id.nav_teste2) {
-
+        } else if (id == R.id.nav_sensor) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.conteiner, new DetalhesSensores())
+                    .commit();
+        } else if (id == R.id.nav_cpu) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.conteiner, new CPU())
+                    .commit();
+        } else if (id == R.id.nav_batteria) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.conteiner, new Bateria())
+                    .commit();
+        } else if (id == R.id.nav_network) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.conteiner, new Internet())
+                    .commit();
+        } else if (id == R.id.nav_sim) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.conteiner, new SIM())
+                    .commit();
+        } else if (id == R.id.nav_camera) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.conteiner, new Camera())
+                    .commit();
+        } else if (id == R.id.nav_memoria) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.conteiner, new Memoria())
+                    .commit();
+        } else if (id == R.id.nav_bluetooth) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.conteiner, new Bluetooth())
+                    .commit();
+        } else if (id == R.id.nav_tela) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.conteiner, new Tela())
+                    .commit();
+        } else if (id == R.id.nav_extra) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.conteiner, new Extras())
+                    .commit();
+        } else if (id == R.id.nav_system_apps) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.conteiner, new APPS())
+                    .commit();
+        } else if (id == R.id.nav_about_us) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.conteiner, new Sobre())
+                    .commit();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
--- app/src/main/java/qualhato/hardwareinfo/fragmentos/Resumo.java
+++ app/src/main/java/qualhato/hardwareinfo/fragmentos/Resumo.java
@@ -34,33 +34,33 @@ public class Resumo extends Fragment {
         View view = inflater.inflate(R.layout.fragment_resumo, container, false);
 
 
-        ListView listaTela = (ListView) view.findViewById(R.id.listaResumo);
-        ArrayList<String> lista = new ArrayList<>();
-        lista.add("Numero de processadores: " + obterHardwarePadrao.quantidadeDeProcessadores());
-        lista.add("total de memoria ram: " + ConverteValores.pegaTamanho(obterHardwarePadrao.totalDeMemoriaRam(getContext())));
-        lista.add("memoria ram Usada : " + ConverteValores.pegaTamanho(obterHardwarePadrao.ramUsada(getContext())));
-        lista.add(" memoria ram livre : " + ConverteValores.pegaTamanho(obterHardwarePadrao.memoriaRamLivre(getContext())));
-        lista.add("Board: " + obterHardwarePadrao.board());
-        lista.add("Fabricação: " + obterHardwarePadrao.fabricacao());
-        lista.add("Host: " + obterHardwarePadrao.host());
-        lista.add("ID: " + obterHardwarePadrao.id());
-        lista.add("Android: " + obterHardwarePadrao.versaoAndroid());
-        lista.add("Marca: " + obterHardwarePadrao.marca());
-        lista.add("Modelo: " + obterHardwarePadrao.modelo());
-        lista.add("Serial: " + obterHardwarePadrao.serial());
-        lista.add("Tipo: " + obterHardwarePadrao.tipo());
-        lista.add("Versão do Sistema:" + obterHardwarePadrao.versaoDoSistema());
-        lista.add("Usuario: " + obterHardwarePadrao.usuario());
-        lista.add("SDK: " + obterHardwarePadrao.versaoSDK());
-        lista.add(obterHardwarePadrao.obterWifi(getContext()));
-        lista.add(obterBateria.readBattery(getContext()));
-
-
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
-                getContext(),
-                android.R.layout.simple_list_item_1, lista);
-
-        listaTela.setAdapter(arrayAdapter);
+//
+        //ListView listaTela = (ListView) view.findViewById(R.id.listaResumo);
+//        ArrayList<String> lista = new ArrayList<>();
+//        lista.add("Numero de processadores: " + obterHardwarePadrao.quantidadeDeProcessadores());
+//        lista.add("total de memoria ram: " + ConverteValores.pegaTamanho(obterHardwarePadrao.totalDeMemoriaRam(getContext())));
+//        lista.add("memoria ram Usada : " + ConverteValores.pegaTamanho(obterHardwarePadrao.ramUsada(getContext())));
+//        lista.add(" memoria ram livre : " + ConverteValores.pegaTamanho(obterHardwarePadrao.memoriaRamLivre(getContext())));
+//        lista.add("Board: " + obterHardwarePadrao.board());
+//        lista.add("Fabricação: " + obterHardwarePadrao.fabricacao());
+//        lista.add("Host: " + obterHardwarePadrao.host());
+//        lista.add("ID: " + obterHardwarePadrao.id());
+//        lista.add("Android: " + obterHardwarePadrao.versaoAndroid());
+//        lista.add("Marca: " + obterHardwarePadrao.marca());
+//        lista.add("Modelo: " + obterHardwarePadrao.modelo());
+//        lista.add("Serial: " + obterHardwarePadrao.serial());
+//        lista.add("Tipo: " + obterHardwarePadrao.tipo());
+//        lista.add("Versão do Sistema:" + obterHardwarePadrao.versaoDoSistema());
+//        lista.add("Usuario: " + obterHardwarePadrao.usuario());
+//        lista.add("SDK: " + obterHardwarePadrao.versaoSDK());
+//        lista.add(obterHardwarePadrao.obterWifi(getContext()));
+//        lista.add(obterBateria.readBattery(getContext()));
+//
+//        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+//                getContext(),
+//                android.R.layout.simple_list_item_1, lista);
+//
+//        listaTela.setAdapter(arrayAdapter);
 
         //retorna o fragmento criado
         return view;
